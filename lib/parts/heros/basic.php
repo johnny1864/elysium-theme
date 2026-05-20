a9531d3ca719e941bdd4620adfca7d72c4e42416
Johnny Rivera <johnny1864g@gmail.com>
2026-05-20 09:52:51 -0700

    updates

--- lib/parts/heros/basic.php
+++ lib/parts/heros/basic.php
@@ -1,16 +1,23 @@
 <section <?php echo $classes; ?>>
-    <div class="container">
-        <div class="hero--basic__content">
-            <h1 class="hero--basic__title"><?php echo $title; ?></h1>
-            <?php if(!empty($hero['content'])):?>
-            <div class="hero--basic__text"><?php echo $hero['content'];  ?></div>
+	<div class="container">
+		<div class="hero--basic__content">
+			<h1 class="hero--basic__title"><?php echo $title; ?></h1>
+            <?php if ( ! empty( $hero['subheading'] ) ) : ?>
+                <p class="hero--basic__subheading">
+                    <?php echo $hero['subheading']; ?>
+                </p>
             <?php endif; ?>
-        </div>
-    </div>
+			<?php if ( ! empty( $hero['content'] ) ) : ?>
+				<div class="hero--basic__text">
+                    <?php echo $hero['content']; ?>
+                </div>
+			<?php endif; ?>
+		</div>
+	</div>
 
-    <div class="hero--basic__image">
-        <?php echo wp_get_attachment_image( $hero['image'], 'full', false, [ 'loading' => 'eager',
-                            'fetchpriority' => 'high',
-                            'decoding' => 'async' ] ); ?>
-    </div>
+	<div class="hero--basic__image">
+		<?php echo wp_get_attachment_image( $hero['image'], 'full', false, [ 'loading' => 'eager',
+			'fetchpriority' => 'high',
+			'decoding' => 'async' ] ); ?>
+	</div>
 </section>
