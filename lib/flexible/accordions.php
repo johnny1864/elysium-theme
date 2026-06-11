0e39bad74c14accca4cb83a221bc16d6dea75fb6
Johnny Rivera <johnny1864g@gmail.com>
2026-06-10 17:14:49 -0700

    updates

--- lib/flexible/accordions.php
+++ lib/flexible/accordions.php
@@ -1,10 +1,18 @@
 <?php
+$bg_image = get_sub_field('bg_image');
 $accordions = get_sub_field( 'accordions' );
 $group = 'accordion_block';
 ?>
 
 <section class="accordions-block">
+    <?php if(!empty($bg_image)) : ?>
+        <?php echo wp_get_attachment_image( $bg_image['ID'], 'full', false, [ 'loading' => 'lazy',
+            'class' => 'accordions-block__bg-image',
+            'fetchpriority' => 'auto',
+            'decoding' => 'async' ] ); ?>
+    <?php else : ?>
     <img loading="lazy" class="accordions-block__bg-image" src="<?php echo esc_url( get_template_directory_uri() . '/dist/images/accordions-bg.webp' ); ?>" alt="">
+    <?php endif ?>
 	<div class="container">
 		<div class="accordions-block__wrapper">
 			<?php foreach ( $accordions as $index => $accordion ) : ?>
@@ -20,6 +28,7 @@ $group = 'accordion_block';
                             <p class="accordion__title">
                                 <?= $title; ?>
                                 <span class="icon-plus">+</span>
+                                <?php if(empty($subtitle)) echo getSVG('underline', false, false); ?>
                             </p>
                             <?php if(!empty($subtitle)) : ?>
                                 <span class="accordion__subtitle">
