de31a0b29265fc6a67cc691ced648ca0c86b8af5
Johnny Rivera <johnny1864g@gmail.com>
2026-08-03 22:06:49 -0700

    updated blog

--- lib/blocks/email-banner.php
+++ lib/blocks/email-banner.php
@@ -1,20 +1,28 @@
+<style>
+    .email-banner {
+        background-image: url(<?php echo esc_url( get_template_directory_uri() . '/dist/images/bricks-bg.webp' ); ?>);
+    }
+</style>
 <section class="email-banner">
     <div class="container">
         <div class="email-banner__grid">
-            <div class="email-banner__icon">
+            <div class="email-banner__icon position-relative">
+                <?= getSVG('mail-accent-left', false, false) ?>
                 <img src="<?php echo esc_url( get_template_directory_uri() . '/dist/images/mail-icon.webp' ); ?>"
 						alt="Mail icon" />
             </div>
             <div class="email-banner__content">
-                <p class="h2">
-                    GET INSIGHTS you’ll
+                <h2 class="h2 email-banner__content-title">
+                    GET INSIGHTS you’ll<br>
                     <span>
                         Actually read.
+                        <?= getSVG('title-underline', false, false) ?>
                     </span>
-                </p>
+                </h2>
             </div>
-            <div class="email-banner__form">
+            <div class="email-banner__form position-relative">
                 <?php echo do_shortcode('[gravityform id="2" title="false"]'); ?>
+                <?= getSVG('mail-accent-right', false, false) ?>
             </div>
         </div>
     </div>
